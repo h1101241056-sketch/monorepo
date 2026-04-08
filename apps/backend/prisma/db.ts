0f0dbdbd375c9826159c11a78cddc327e8cf9055
h1101241056-sketch <h1101241056@student.untan.ac.id>
2026-04-08 23:57:18 +0700

    init

--- apps/backend/prisma/db.ts
+++ apps/backend/prisma/db.ts
@@ -1,8 +1,13 @@
-import { PrismaLibSql } from "@prisma/adapter-libsql";
 import { PrismaClient } from "../src/generated/prisma/client";
+import { PrismaLibSql } from "@prisma/adapter-libsql";
+import path from "path";
+
+const dbUrl =
+  process.env.DATABASE_URL ||
+  `file:${path.resolve(__dirname, "../dev.db")}`;
 
 const adapter = new PrismaLibSql({
-  url: process.env.DATABASE_URL!,
+  url: dbUrl,
   authToken: process.env.DB_AUTH_TOKEN,
 });
 
